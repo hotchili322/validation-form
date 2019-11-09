15f7f6b282fe20649e12ac831d5e4db31cb81ca4
hotchili322 <houman.alvandi@gmail.com>
2019-11-09 16:57:46 +0330

    add bootstrap add jquery

--- index-view.php
+++ index-view.php
@@ -5,9 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/stylesheet.css">
     <title>Form Validation</title>
 </head>
 <body>
 
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
